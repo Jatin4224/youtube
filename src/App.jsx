af2ffc1e073072cbeeaf040f1dcf4dc21b3c02fe
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-01-21 03:12:39 +0530

    creeated coponents

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// App.js
+import React from "react";
+import Head from "./components/Head";
+import Body from "./components/Body";
+
+function App() {
+  return (
+    <div>
+      <Head />
+      <Body />
+      {/* Other components */}
+    </div>
+  );
+}
+
+export default App;
