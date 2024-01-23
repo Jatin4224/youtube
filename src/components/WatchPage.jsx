f53f74c4a7c18e69169dc73e72f5ebab11c1e741
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-01-24 03:03:14 +0530

    bug fix

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -19,7 +19,7 @@ const WatchPage = () => {
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullscreen
+        allowFullScreen
       ></iframe>
     </div>
   );
