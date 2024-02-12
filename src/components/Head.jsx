8d2ba78cc3cf6b941f9d8b367b2d335d8167b159
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-02-12 21:37:09 +0530

    fix bug

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -11,11 +11,10 @@ const Head = () => {
       clearTimeout(timer);
     };
   }, [searchQuery]);
-
   const getSearchSuggestions = async () => {
     const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
     const json = await data.json();
-    // console.log(json[1]);
+    console.log(json);
   };
 
   const dispatch = useDispatch();
